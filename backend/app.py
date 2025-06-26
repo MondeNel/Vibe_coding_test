2287dac941ea9f72a9b3bbe4d19bea4ebda10cac
Monde Nel <mondenel1996@gmail.com>
2025-06-26 13:21:15 +0200

    added the directory direction to the frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from routes import register_routes
+import os
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=os.path.join('..', 'frontend', 'templates'),
+    static_folder=os.path.join('..', 'frontend', 'static')
+)
 register_routes(app)
 
 if __name__ == '__main__':
